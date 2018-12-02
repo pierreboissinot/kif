35312e21cfb08f1b0523a5e524115597dcb3712c
Pierre Boissinot <perso@pierreboissinot.me>
2018-12-02 01:12:29 +0100

    feat(import): Import Wrike task to Gitlab issue

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
 # Kif
 
+## Usage
+### Import a Wrike task into Gitlab issue
+```
+kif import https://www.wrike.com/open.htm\?id\=83740872A
+```
+[demo](https://asciinema.org/a/7j5KmkLIGYPK0Kp4CmgVhndKm)
+
+## Configuration
+Add `~/.kif.toml` like:
+```toml
+wrikeApiToken="my.wrike.token"
+gitlabApiToken="my.gitlab.token"
+```
+
 ## Motivation
 
 At work, we use Wrike to manage tasks. It's good enough to be used by
@@ -9,3 +23,8 @@ When a task arrives in developer Inbox, he creates an associated Gitlab issue.
 If the task is well described, he copies paste the title and the description;
 if not, the developer has to adapt the title and description to correct
 infos or split a Wrike task into multiple issues.
+
+## Credits
+
+- [issues-helper](https://www.clever-cloud.com/blog/features/2018/02/13/issues-helper/)
+- [cobra](https://github.com/spf13/cobra)
---
+++ cmd/import.go
@@ -0,0 +1,94 @@
+// Copyright © 2018 Pierre Boissinot <perso@pierreboissinot.me>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+package cmd
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"github.com/xanzy/go-gitlab"
+	"go-wrike"
+	"log"
+	"os/exec"
+	"regexp"
+	"strings"
+)
+
+// importCmd represents the import command
+var importCmd = &cobra.Command{
+	Use:   "import",
+	Short: "Import issue from Wrike task",
+	Long: `Open gitlab issue from Wrike task, example:
+	kif import $WRIKE_PERMALING
+
+	The Gitlab project is defined by the remote origin
+	of your current git repository.`,
+	Args: cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		permalink := args[0]
+		wrikeApiToken := viper.GetString("wrikeApiToken")
+		wrikeClient := wrike.NewClient(nil, wrikeApiToken)
+		fields := "[\"description\"]"
+		tasks, _, err := wrikeClient.Tasks.QueryTasks(wrike.QueryTasksOptions{permalink, fields})
+		if err != nil {
+			fmt.Errorf("No results for permalink %v", permalink)
+		}
+
+		if len(tasks.Data) != 1 {
+			fmt.Errorf("More than 1 task returned: %v", len(tasks.Data))
+		}
+		task := tasks.Data[0]
+		cmdName := "git"
+		cmdArgs := []string{"remote", "get-url", "origin"}
+		gitCmd := exec.Command(cmdName, cmdArgs...)
+		var out bytes.Buffer
+		gitCmd.Stdout = &out
+		gitErr := gitCmd.Run()
+		if gitErr != nil {
+			log.Fatal(gitErr)
+		}
+		re := regexp.MustCompile(`(?:git|ssh|https?|git@[-\w.]+):(?P<Pid>(\/\/)?(.*?))(\.git)(\/?|\#[-\d\w._]+?)`)
+		/*
+			fmt.Printf("%#v\n", re.FindStringSubmatch(out.String()))
+			fmt.Printf("%#v\n", re.SubexpNames())
+			fmt.Println(re.SubexpNames()[1])
+		*/
+		format := fmt.Sprintf("${%s}", re.SubexpNames()[1])
+		pid := strings.TrimSuffix(re.ReplaceAllString(out.String(), format), "\n")
+		gitlabClient := gitlab.NewClient(nil, viper.GetString("gitlabApiToken"))
+		i := &gitlab.CreateIssueOptions{Title: gitlab.String(task.Title), Description: gitlab.String(task.Description)}
+		issue, _, err := gitlabClient.Issues.CreateIssue(pid, i)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("Created issue: %v\nWith description: %s\nView issue: %s\n", issue.ID, issue.Description, issue.WebURL)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(importCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// importCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// importCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
